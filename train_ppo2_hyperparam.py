2cc09bc6f228ef65d0a55cf51d6b6b99ecbe167d
WilliamC Eustis <willceusits@gmail.com>
2020-11-13 15:01:35 -0600

    updated tuning script

--- train_ppo2_hyperparam.py
+++ train_ppo2_hyperparam.py
@@ -1,11 +1,12 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from stable_baselines.common.callbacks import CheckpointCallback
 from stable_baselines.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise, AdaptiveParamNoiseSpec
 from stable_baselines.common.vec_env import DummyVecEnv,SubprocVecEnv
 
 from stable_baselines import DQN, DDPG, TD3, PPO2
-
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 from stable_baselines.td3.policies import MlpPolicy as td3MLP
 from stable_baselines.td3.policies import LnMlpPolicy as td3LnMlp
@@ -55,9 +56,9 @@ def sample_ddpg_params(trial):
     # actor_lr = trial.suggest_loguniform('actor_lr', 1e-5, 1)
     # critic_lr = trial.suggest_loguniform('critic_lr', 1e-5, 1)
     learning_rate = trial.suggest_loguniform('lr', 1e-5, 1)
-    batch_size = trial.suggest_categorical('batch_size', [16, 32, 64, 128, 256])
+    batch_size = trial.suggest_categorical('batch_size', [16, 32, 64, 100, 128, 256, 512])
     buffer_size = trial.suggest_categorical('memory_limit', [int(1e4), int(1e5), int(1e6)])
-    noise_type = trial.suggest_categorical('noise_type', ['ornstein-uhlenbeck', 'normal', 'adaptive-param'])
+    noise_type = trial.suggest_categorical('noise_type', ['ornstein-uhlenbeck', 'normal'])
     noise_std = trial.suggest_uniform('noise_std', 0, 1)
     normalize_observations = trial.suggest_categorical('normalize_observations', [True, False])
     normalize_returns = trial.suggest_categorical('normalize_returns', [True, False])
@@ -72,12 +73,8 @@ def sample_ddpg_params(trial):
         'normalize_returns': normalize_returns
     }
 
-    if noise_type == 'adaptive-param':
-        hyperparams['param_noise'] = AdaptiveParamNoiseSpec(initial_stddev=noise_std,
-                                                            desired_action_stddev=noise_std)
-        # Apply layer normalization when using parameter perturbation
-        hyperparams['policy_kwargs'] = dict(layer_norm=True)
-    elif noise_type == 'normal':
+
+    if noise_type == 'normal':
         hyperparams['action_noise'] = NormalActionNoise(mean=np.zeros(1),
                                                         sigma=noise_std * np.ones(1))
     elif noise_type == 'ornstein-uhlenbeck':
@@ -130,30 +127,32 @@ class Objective(object):
         env_id = 'gym_custom:fooCont-v0'
         env = gym.make(env_id, data=self.train_data)
         env = DummyVecEnv([lambda: env])
-
-        algo = trial.suggest_categorical('algo', ['PPO2', 'DQN', 'DDPG', 'TD3'])
+        algo = trial.suggest_categorical('algo', ['PPO2', 'DDPG', 'TD3'])
         model = 0
         if algo == 'PPO2':
 
-            policy = trial.suggest_categorical('policy', [commonMlp, commonMlpLstm])
+            policy_choice = trial.suggest_categorical('policy', [False, True])
+            policy = commonMlp if policy_choice else commonMlpLstm
             model_params = optimize_ppo2(trial)
 
             model = PPO2(policy, env, verbose=0, nminibatches=1, **model_params)
-            model.learn(1000000)
+            model.learn(276*7000*3)
 
         elif algo == 'DDPG':
-            policy = trial.suggest_categorical('policy', [ddpgMlp, ddpgLnMlp])
+            policy_choice = trial.suggest_categorical('policy', [False, True])
+            policy = ddpgLnMlp
             model_params = sample_ddpg_params(trial)
 
             model= DDPG(policy, env, verbose=0, **model_params)
-            model.learn(1000000)
+            model.learn(276*7000*3)
 
         elif algo == 'TD3':
-            policy = trial.suggest_categorical('policy', [td3MLP, td3LnMlp])
+            policy_choice = trial.suggest_categorical('policy', [False, True])
+            policy = td3MLP if policy_choice else td3LnMlp
             model_params = sample_td3_params(trial)
 
             model = TD3(policy, env, verbose=0, **model_params)
-            model.learn(1000000)
+            model.learn(276*7000*3)
 
         rewards = []
         reward_sum = 0.0
@@ -161,21 +160,23 @@ class Objective(object):
         env = DummyVecEnv([lambda: env])
 
         obs = env.reset()
-        for i in range(100):
-            action, _ = model.predict(obs)
-            obs, reward, done, _ = env.step(action)
-            reward_sum += reward
+        for ep in range(150):
+            for step in range(276):
+                action, _ = model.predict(obs)
+                obs, reward, done, _ = env.step(action)
+                reward_sum += reward
 
-            if done:
-                rewards.append(reward_sum)
-                reward_sum = 0.0
-                obs = env.reset()
+                if done:
+                    rewards.append(reward_sum)
+                    reward_sum = 0.0
+                    obs = env.reset()
+
         reward_total = np.mean(rewards)
         return reward_total * -1
 
 if __name__ == '__main__':
     study = optuna.create_study()
-    study.optimize(Objective(train_data, test_data), n_trials=500)
+    study.optimize(Objective(train_data, test_data), n_trials=50)
     joblib.dump(study, 'study.pkl')
     print('Best trial until now:')
     print(' Value: ', study.best_trial.value)
